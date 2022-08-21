6362bc583dbddf748ef2ccb953628a7babc1c4a7
Anon4Now <tutkoda@gmail.com>
2022-08-21 10:51:20 -0400

    testing main

--- main.py
+++ main.py
@@ -11,34 +11,38 @@ from utils import (
     create_boto3_resource,
     create_boto3_client
 )
+from default_resources import (
+    GetResources
+)
 
 #  VPC resources created by AWS 'https://docs.aws.amazon.com/vpc/latest/userguide/default-vpc.html'
 
 logger = create_logger()
-regionClient = boto3.client('ec2')
-regionResources = GetRegions(regionClient)
-regions = regionResources.getRegions()
+# regionClient = boto3.client('ec2')
+# regionResources = GetRegions(regionClient)
+# regions = regionResources.getRegions()
 
 
 #  Main Function
 def main():
-    outArgs = getArgs()
-    for region in regions:
-        try:
-            vpcClient = boto3.client('ec2', region_name=region)
-            ec2 = boto3.resource('ec2', region_name=region)
-            optionSet = SetArgsAndObjects(vpcClient, ec2, region)
-
-            if outArgs.sanitize_option == "all":
-                optionSet.allOptions()
-            elif outArgs.sanitize_option == "deleteonly":
-                optionSet.deleteResourcesOption()
-            elif outArgs.sanitize_option == "modifyonly":
-                optionSet.modifyResourceOption()
-            else:
-                logger.error("[-] The option provided was not found, please try again (--help for more info)")
-        except Exception as e:
-            logger.error("[-] Failed to create variables with error: %s", e)
+    get_resources = GetResources
+    # outArgs = getArgs()
+    # for region in regions:
+    #     try:
+    #         vpcClient = boto3.client('ec2', region_name=region)
+    #         ec2 = boto3.resource('ec2', region_name=region)
+    #         optionSet = SetArgsAndObjects(vpcClient, ec2, region)
+    #
+    #         if outArgs.sanitize_option == "all":
+    #             optionSet.allOptions()
+    #         elif outArgs.sanitize_option == "deleteonly":
+    #             optionSet.deleteResourcesOption()
+    #         elif outArgs.sanitize_option == "modifyonly":
+    #             optionSet.modifyResourceOption()
+    #         else:
+    #             logger.error("[-] The option provided was not found, please try again (--help for more info)")
+    #     except Exception as e:
+    #         logger.error("[-] Failed to create variables with error: %s", e)
 
 
 if __name__ == "__main__":
