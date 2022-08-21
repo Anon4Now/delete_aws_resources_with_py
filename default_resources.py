65b30b20fe34dc23908b24cd40cc6eeb5ebe4b89
Anon4Now <tutkoda@gmail.com>
2022-08-21 10:40:22 -0400

    created class structure

--- default_resources.py
+++ default_resources.py
@@ -1,19 +1,41 @@
 """Module containing classes that are used to find and delete resources"""
 
 # Standard Library imports
+from dataclasses import dataclass
 
 # Third-party imports
+from botocore.exceptions import ClientError
 
 # Local app imports
 from utils import (
-    create_logger
+    create_logger,
+    create_boto3_client,
+    create_boto3_resource
 )
 
 logger = create_logger()
 
 
+@dataclass
 class DefaultResources:
-    pass
+    region_list: list
+    vpc_list: list
+    skip_regions: list = None
+
+    def __init__(self):
+        pass
+
+    def get_regions(self) -> list:
+        """
+        Will generate a list of regions to take action on
+        :return: List containing the regions to delete resources from
+        :raise: AWS API "Boto3" ClientErrors
+        """
+        try:
+            get_regions = create_boto3_client('ec2').describe_regions()
+            print("DELETE ME AFTER USE IN DATACLASS", get_regions)
+        except ClientError as e:
+            logger.error("[-] Failed to retrieve region object from AWS with error: %s", e)
 
 
 class GetResources(DefaultResources):
