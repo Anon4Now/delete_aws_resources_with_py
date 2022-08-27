32bc529c7c3ed00c81c98a8dd6014068d3e013cc
Anon4Now <tutkoda@gmail.com>
2022-08-27 10:37:35 -0400

    added comments to delete_resources func

--- main.py
+++ main.py
@@ -17,26 +17,39 @@ logger = create_logger()
 SKIP_REGIONS = ["us-east-1", "us-west-2"]
 
 
-def delete_resources(obj):
+def delete_resources(obj: Resources) -> None:
+    """
+    Function that takes in an instantiated Resources object and uses attrs on object
+    to perform detach/delete actions related to default VPCs
+    :param obj: Instantiated Resource Class object passed from main()
+    :return: None
+    """
+    """Actions related to Internet Gateway removal from default VPC"""
     for igw in obj.igw:
         logger.info("[!] Attempting to detach and delete IGW-ID: '%s' in Region: '%s'", igw.id, obj.region)
         igw.detach_from_vpc(VpcId=obj.vpc_id)
         igw.delete()
         logger.info("[+] '%s' in Region: '%s' was successfully detached and deleted\n", igw.id, obj.region)
+
+    """Actions related to Subnet removal from default VPC"""
     for subnet in obj.default_subnets:
         logger.info("[!] Attempting to detach and delete Subnet-ID: '%s' in Region: '%s'", subnet.id, obj.region)
         subnet.delete()
         logger.info("[+] '%s' in Region: '%s' was successfully detached and deleted\n", subnet.id, obj.region)
-    for routeTable in obj.route_tables:
+
+    """Actions related to RouteTable removal from default VPC"""
+    for route_table in obj.route_tables:
         #  found the route table associations
-        associatedAttribute = [routeTable.associations_attribute for routeTable in obj.route_tables]
+        associated_attribute = [routeTable.associations_attribute for routeTable in obj.route_tables]
         #  check to see if it is the default the route table, this cannot be removed
-        if [rtb_ass[0]['RouteTableId'] for rtb_ass in associatedAttribute if rtb_ass[0]['Main'] is True]:
-            logger.info("[!] '%s'' is the main route table, this cannot be removed continuing\n", routeTable.id)
+        if [rtb_ass[0]['RouteTableId'] for rtb_ass in associated_attribute if rtb_ass[0]['Main'] is True]:
+            logger.info("[!] '%s'' is the main route table, this cannot be removed continuing\n", route_table.id)
             continue
-        logger.info("[!] Attempting to detach and delete Subnet-ID: '%s' in Region: '%s'", routeTable.id, obj.region)
-        obj.boto_resource.RouteTable(routeTable.id).delete()
-        logger.info("[+] '%s' in Region: '%s' was successfully detached and deleted\n", routeTable.id, obj.region)
+        logger.info("[!] Attempting to detach and delete Subnet-ID: '%s' in Region: '%s'", route_table.id, obj.region)
+        obj.boto_resource.RouteTable(route_table.id).delete()
+        logger.info("[+] '%s' in Region: '%s' was successfully detached and deleted\n", route_table.id, obj.region)
+
+    """Actions related to NACL removal from default VPC"""
     for acl in obj.acls:
         if acl.is_default:
             logger.info("[!] '%s' is the default NACL, this cannot be removed continuing\n")
@@ -45,6 +58,8 @@ def delete_resources(obj):
             logger.info("[!] Attempting to remove NACL-ID: '%s' for Region: '%s'", acl.id, obj.region)
             acl.delete()
             logger.info("[+] '%s' in Region: '%s' was successfully detached and deleted\n", acl.id, obj.region)
+
+    """Actions related to SG removal from default VPC"""
     for sg in obj.sgs:
         if sg.group_name == 'default':
             logger.info("[!] '%s' is the default SG, this cannot be removed continuing\n")
@@ -53,6 +68,8 @@ def delete_resources(obj):
             logger.info("[!] Attempting to remove SG-ID: '%s' for Region: '%s'", sg.id, obj.region)
             sg.delete()
             logger.info("[+] '%s' in Region: '%s' was successfully detached and deleted\n", sg.id, obj.region)
+
+    """Actions related to the deletion of the default VPC"""
     logger.info("[!] Attempting to remove VPC-ID: '%s' for Region: '%s'", obj.vpc_id, obj.region)
     obj.current_vpc_resource.delete()
     logger.info("[+] '%s' in Region: '%s' was successfully detached and deleted\n\n", obj.vpc_id, obj.region)
@@ -107,20 +124,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-    # outArgs = getArgs()
-    # for region in regions:
-    #     try:
-    #         vpcClient = boto3.client('ec2', region_name=region)
-    #         ec2 = boto3.resource('ec2', region_name=region)
-    #         optionSet = SetArgsAndObjects(vpcClient, ec2, region)
-    #
-    #         if outArgs.sanitize_option == "all":
-    #             optionSet.allOptions()
-    #         elif outArgs.sanitize_option == "deleteonly":
-    #             optionSet.deleteResourcesOption()
-    #         elif outArgs.sanitize_option == "modifyonly":
-    #             optionSet.modifyResourceOption()
-    #         else:
-    #             logger.error("[-] The option provided was not found, please try again (--help for more info)")
-    #     except Exception as e:
-    #         logger.error("[-] Failed to create variables with error: %s", e)
