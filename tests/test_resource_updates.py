cf28b8e9bedd0d8f59f08d285d83180ce37454fa
Anon4Now <tutkoda@gmail.com>
2022-10-02 18:19:00 -0400

    added some more tests for a little more coverage

--- tests/test_resource_updates.py
+++ tests/test_resource_updates.py
@@ -1,7 +1,5 @@
 """Module containing tests for Delete and Update classes"""
 
-import pytest
-
 # Local App imports
 from delete_aws_resources_with_py.resource_updates import (
     Delete,
@@ -19,18 +17,17 @@ def test_update_class_nacl_rules(get_resource_obj):
     assert update_res.update_nacl_rules() is True
 
 
-@pytest.mark.skip(reason="describe_security_group_rules doesn't exist in moto yet, can't mock")
-def test_update_class_sg_rules(get_resource_obj):
+def test_update_class_sg_rules(get_resource_obj, mocker, egress_rule):
     update_res = Update(get_resource_obj)
-    assert update_res.update_sg_rules() is True
-
-
-@pytest.mark.skip(reason="get_service_setting & update_service_setting doesn't exist in moto yet, can't mock")
-def test_update_ssm_preferences(ec2_client):
-    assert Update.update_ssm_preferences(ec2_client, region='us-east-1')
-
-
-@pytest.mark.skip(reason="going to fail due to missing moto services")
-def test_run_update(get_resource_obj):
-    update_res = Update(get_resource_obj)
+    mocker.patch.object(
+        update_res,
+        'get_sg_rules',
+        return_value=egress_rule
+    )
+    mocker.patch.object(
+        update_res,
+        'update_sg_rules',
+        return_value=True
+    )
     assert update_res.run_update() is True
+    assert update_res.update_sg_rules() is True
