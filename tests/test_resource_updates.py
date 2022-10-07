de689bf2ef37bf8ca2556c70b48227302644314f
Anon4Now <tutkoda@gmail.com>
2022-10-06 22:48:28 -0400

    built tests for resource_updates module

--- tests/test_resource_updates.py
+++ tests/test_resource_updates.py
@@ -2,19 +2,14 @@
 
 # Local App imports
 from delete_aws_resources_with_py.resource_updates import (
-    Delete,
-    Update
+    UpdateNaclResource,
+    UpdateSgResource
 )
 from delete_aws_resources_with_py.main import execute_changes_on_resources
 
 
-def test_delete_class(get_resource_obj):
-    del_res = Delete(get_resource_obj)
-    assert del_res.run_delete() is True
-
-
 def test_update_class_nacl_rules(get_resource_obj):
-    update_res = Update(get_resource_obj)
+    update_res = UpdateNaclResource(get_resource_obj)
     assert update_res.update_nacl_rules() is True
 
 
@@ -22,29 +17,13 @@ def test_update_sg_rules(get_resource_obj, mocker, egress_rule, ssm_client):
     def mock_get_sg_rules(*args, **kwargs):
         return egress_rule
 
-    mocker.patch('delete_aws_resources_with_py.main.Update.get_sg_rules', mock_get_sg_rules)
+    mocker.patch('delete_aws_resources_with_py.main.UpdateSgResource.get_sg_rules', mock_get_sg_rules)
 
     def mock_revoke_sg_rule(*args, **kwargs):
         return {'Return': True}
 
-    mocker.patch('delete_aws_resources_with_py.main.Update.revoke_ingress_sg_rule', mock_revoke_sg_rule)
-    mocker.patch('delete_aws_resources_with_py.main.Update.revoke_egress_sg_rule', mock_revoke_sg_rule)
-    print(get_resource_obj.sgs)
+    mocker.patch('delete_aws_resources_with_py.main.UpdateSgResource.revoke_ingress_sg_rule', mock_revoke_sg_rule)
+    mocker.patch('delete_aws_resources_with_py.main.UpdateSgResource.revoke_egress_sg_rule', mock_revoke_sg_rule)
 
     actual = execute_changes_on_resources('modify', get_resource_obj, ssm_client, 'us-east-1')
     assert actual is True
-
-# def test_update_class_sg_rules(get_resource_obj, mocker, egress_rule):
-#     update_res = Update(get_resource_obj)
-#     mocker.patch.object(
-#         update_res,
-#         'get_sg_rules',
-#         return_value=egress_rule
-#     )
-#     mocker.patch.object(
-#         update_res,
-#         'update_sg_rules',
-#         return_value=True
-#     )
-#     assert update_res.run_update() is True
-#     assert update_res.update_sg_rules() is True
