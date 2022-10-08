302ad7a648f9a9bd9f606c29f4f3696353d05414
Anon4Now <tutkoda@gmail.com>
2022-10-08 10:41:58 -0400

    removed unused code

--- delete_aws_resources_with_py/main.py
+++ delete_aws_resources_with_py/main.py
@@ -76,25 +76,6 @@ def main():
         except NoDefaultVpcExistsError:
             logger.info("[!] Region: '%s' does not have a default VPC, continuing", current_region)
             continue
-            # if args.sanitize_option not in ['delete', 'modify']:  # validate cmd line arg
-            #     logger.error("[-] Entered an incorrect option, use -h or --help for more information")
-            #     break
-            # elif not obj.vpc_id:
-            #     logger.info("[!] Region: '%s' does not have a default VPC, continuing", current_region)
-            #     continue
-            # else:
-            #     logger.info("[!] Performing '%s' actions on region: '%s'", args.sanitize_option, current_region)
-            #     logger.info("========================================================================================\n")
-            #     if args.sanitize_option == 'delete':
-            #         Update.update_ssm_preferences(boto_client=ssm_client, region=current_region)
-            #         del_resource = Delete(obj)
-            #         if del_resource.run_delete():
-            #             logger.info("[+] **All VPC delete actions successfully performed in '%s' region**", current_region)
-            #     elif args.sanitize_option == "modify":
-            #         Update.update_ssm_preferences(boto_client=ssm_client, region=current_region)
-            #         update_resource = Update(obj)
-            #         if update_resource.run_update():
-            #             logger.info("[+] **All VPC update actions successfully performed in '%s' region**\n\n", current_region)
 
 
 if __name__ == "__main__":
