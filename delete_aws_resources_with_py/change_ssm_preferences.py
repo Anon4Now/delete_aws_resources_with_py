8bbe4b50c3d12d7783e58330f3a1719873a88791
Anon4Now <tutkoda@gmail.com>
2022-10-09 13:46:34 -0400

    refactored to a class and confirmed working

--- delete_aws_resources_with_py/change_ssm_preferences.py
+++ delete_aws_resources_with_py/change_ssm_preferences.py
@@ -5,35 +5,41 @@ from typing import Any
 from delete_aws_resources_with_py.utils import logger
 
 
-def get_current_service_setting(ssm_client: Any, share_setting: str) -> str:
-    """Check to see what the current service setting is."""
+class SsmPreference:
+    def __init__(self, ssm_client: Any, region: str) -> None:
+        self.ssm_client = ssm_client
+        self.region = region
+        self.public_share_setting = "/ssm/documents/console/public-sharing-permission"
 
-    return ssm_client.get_service_setting(SettingId=share_setting)['ServiceSetting']['SettingValue']
+    def __repr__(self):
+        return f'SsmPreference({self.ssm_client}, {self.region})'  # pragma: no cover
 
+    def get_current_service_setting_check(self) -> bool:
+        """Check to see what the current service setting is."""
+        return self.ssm_client.get_service_setting(SettingId=self.public_share_setting)['ServiceSetting'][
+                   'SettingValue'] == 'Enable'  # pragma: no cover - moto mock not implemented
 
-def update_public_service_setting(ssm_client: Any, share_setting: str, share_setting_status: str) -> int:
-    """Make changes to the share service setting"""
-    return ssm_client.update_service_setting(SettingId=share_setting, SettingValue=share_setting_status)['ResponseMetadata']['HTTPStatusCode']
+    def update_public_service_setting_check(self, share_setting_status: str) -> bool:
+        """Make changes to the share service setting"""
+        return self.ssm_client.update_service_setting(
+            SettingId=self.public_share_setting,
+            SettingValue=share_setting_status)['ResponseMetadata']['HTTPStatusCode'] \
+            == 200  # pragma: no cover - moto mock not implemented
 
+    def check_ssm_preferences(self) -> None:
+        """
+        Function designed to check the SSM document preferences in each region.
+        If they are open to the public, update the preference to be private to the account.
 
-def update_ssm_preferences(boto_client, region) -> None:
-    """
-    Function designed to check the SSM document preferences in each region.
-    If they are open to the public, update the preference to be private to the account.
-    :param boto_client: (required) Instantiated boto3 client for the current region in the loop
-    :param region: (required) Current region in the loop passed from main()
-    :return: Boolean depicting the success of the actions (True = success/False = failure)
-    """
-    public_share_setting = "/ssm/documents/console/public-sharing-permission"  # path provided by BOTO3 API docs
-    status = 200
-    current_setting = get_current_service_setting(boto_client, public_share_setting)
-    if current_setting != 'Enable':
-        logger.info("[+] SSM Document preferences already block public access with status '%s', no action taken\n",
-                    current_setting)
-        return
+        """
 
-    logger.info(
-        "[!] SSM Document preferences allows public access with status '%s' in Region: '%s', attempting to update",
-        current_setting, region)
-    if update_public_service_setting(boto_client, public_share_setting, 'Disable') == status:
-        logger.info("[+] Preferences successfully updated to 'Disable' in Region: '%s'\n", region)
+        if not self.get_current_service_setting_check():
+            logger.info(
+                "[+] SSM Document preferences already block public access with status 'Disable', no action taken\n")
+            return
+        logger.info(
+            "[!] SSM Document preferences allows public access with status 'Enable' in Region: '%s', attempting to update",
+            self.region)
+        if self.update_public_service_setting_check:
+            logger.info("[+] Preferences successfully updated to 'Disable' in Region: '%s'\n", self.region)
+            return
