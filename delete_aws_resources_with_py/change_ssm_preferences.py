18b61e966f6c13d27af53fc67a11d513174979ff
Anon4Now <tutkoda@gmail.com>
2022-10-09 10:59:41 -0400

    refactored to SRP funcs

--- delete_aws_resources_with_py/change_ssm_preferences.py
+++ delete_aws_resources_with_py/change_ssm_preferences.py
@@ -1,7 +1,19 @@
 """Module containing functions to change SSM preferences to private"""
+from typing import Any
 
 # Local App imports
 from delete_aws_resources_with_py.utils import logger
+
+
+def get_current_service_setting(ssm_client: Any, share_setting: str) -> str:
+    """Check to see what the current service setting is."""
+
+    return ssm_client.get_service_setting(SettingId=share_setting)['ServiceSetting']['SettingValue']
+
+
+def update_public_service_setting(ssm_client: Any, share_setting: str, share_setting_status: str) -> int:
+    """Make changes to the share service setting"""
+    return ssm_client.update_service_setting(SettingId=share_setting, SettingValue=share_setting_status)['ResponseMetadata']['HTTPStatusCode']
 
 
 def update_ssm_preferences(boto_client, region) -> None:
@@ -13,15 +25,15 @@ def update_ssm_preferences(boto_client, region) -> None:
     :return: Boolean depicting the success of the actions (True = success/False = failure)
     """
     public_share_setting = "/ssm/documents/console/public-sharing-permission"  # path provided by BOTO3 API docs
-    get_current_acct_settings = boto_client.get_service_setting(
-        SettingId=public_share_setting)  # check current setting
-    current_setting = get_current_acct_settings['ServiceSetting']['SettingValue']
-    if current_setting == 'Enable':
-        logger.info(
-            "[!] SSM Document preferences allows public access with status '%s' in Region: '%s', attempting to update",
-            current_setting, region)
-        boto_client.update_service_setting(SettingId=public_share_setting, SettingValue="Disable")  # update setting
-        logger.info("[+] Preferences successfully updated to 'Disable' in Region: '%s'\n", region)
-    else:
+    status = 200
+    current_setting = get_current_service_setting(boto_client, public_share_setting)
+    if current_setting != 'Enable':
         logger.info("[+] SSM Document preferences already block public access with status '%s', no action taken\n",
                     current_setting)
+        return
+
+    logger.info(
+        "[!] SSM Document preferences allows public access with status '%s' in Region: '%s', attempting to update",
+        current_setting, region)
+    if update_public_service_setting(boto_client, public_share_setting, 'Disable') == status:
+        logger.info("[+] Preferences successfully updated to 'Disable' in Region: '%s'\n", region)
