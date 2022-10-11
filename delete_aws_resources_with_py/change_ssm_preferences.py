09ecb7e17f2053c1f28e8e2ce9831c5a4b24b41f
Anon4Now <tutkoda@gmail.com>
2022-10-11 19:23:18 -0400

    added doc string comments

--- delete_aws_resources_with_py/change_ssm_preferences.py
+++ delete_aws_resources_with_py/change_ssm_preferences.py
@@ -1,4 +1,6 @@
-"""Module containing functions to change SSM preferences to private"""
+"""Module containing functions to change SSM preferences to private."""
+
+# Standard Library imports
 from typing import Any
 
 # Local App imports
@@ -6,7 +8,14 @@ from delete_aws_resources_with_py.utils import logger
 
 
 class SsmPreference:
+    """Action-oriented class to check and update the region SSM settings."""
     def __init__(self, ssm_client: Any, region: str) -> None:
+        """
+        Initializer that takes in two params.
+
+        :param ssm_client: (required) An instantiated boto3 client for SSM
+        :param region: (required) A string value containing the current region
+        """
         self.ssm_client = ssm_client
         self.region = region
         self.public_share_setting = "/ssm/documents/console/public-sharing-permission"
@@ -15,12 +24,21 @@ class SsmPreference:
         return f'SsmPreference({self.ssm_client}, {self.region})'  # pragma: no cover
 
     def get_current_service_setting_check(self) -> bool:
-        """Check to see what the current service setting is."""
+        """
+        Check to see what the current service setting is.
+
+        :return A boolean result that represents if the region already has blocked access
+        """
         return self.ssm_client.get_service_setting(SettingId=self.public_share_setting)['ServiceSetting'][
                    'SettingValue'] == 'Enable'
 
     def update_public_service_setting_check(self, share_setting_status: str) -> bool:
-        """Make changes to the share service setting"""
+        """
+        Make changes to the share service setting in region.
+
+        :param share_setting_status: (required) A string value containing the status value to be passed in API call
+        :return A boolean result that represents whether the change occurred successfully
+        """
         return self.ssm_client.update_service_setting(
             SettingId=self.public_share_setting,
             SettingValue=share_setting_status)['ResponseMetadata']['HTTPStatusCode'] \
@@ -29,7 +47,9 @@ class SsmPreference:
     def check_ssm_preferences(self) -> None:
         """
         Function designed to check the SSM document preferences in each region.
+
         If they are open to the public, update the preference to be private to the account.
+        :return None
 
         """
 
