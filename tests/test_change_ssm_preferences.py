9ceedc6ae25fe80c6a3963ac5e94edb2430cd4d9
Anon4Now <tutkoda@gmail.com>
2022-10-10 20:03:58 -0400

    refactored testing code for 90% cov

--- tests/test_change_ssm_preferences.py
+++ tests/test_change_ssm_preferences.py
@@ -3,41 +3,8 @@
 from delete_aws_resources_with_py.change_ssm_preferences import SsmPreference
 
 
-def test_get_current_service_setting(ssm_client, mocker):
-    """Test getting the current service setting"""
-
-    def mock_get_current_service_setting(*args, **kwargs):
-        return {}
-
-    mocker.patch('tests.test_change_ssm_preferences.SsmPreference.get_current_service_setting_check',
-                 mock_get_current_service_setting)
-
-    assert SsmPreference(ssm_client, 'us-east-1').get_current_service_setting_check() == {}
-
-
-def test_ssm_preference_class_enable_block(ssm_client, mocker):
-    """Test the class to update the preferences."""
-
-    def mock_get_current_service_setting(*args):
-        return True
-
-    mocker.patch('delete_aws_resources_with_py.main.SsmPreference.get_current_service_setting_check',
-                 mock_get_current_service_setting)
-
-    def mock_update_public_service_setting(*args):
-        return True
-
-    mocker.patch('delete_aws_resources_with_py.main.SsmPreference.update_public_service_setting_check',
-                 mock_update_public_service_setting)
-
-    assert SsmPreference(ssm_client, 'us-east-1').check_ssm_preferences() is None
-
-
-def test_ssm_preference_class_no_block(ssm_client, mocker):
-    def mock_get_current_service_setting(*args):
-        return False
-
-    mocker.patch('delete_aws_resources_with_py.main.SsmPreference.get_current_service_setting_check',
-                 mock_get_current_service_setting)
-
-    assert SsmPreference(ssm_client, 'us-east-1').check_ssm_preferences() is None
+def test_get_current_service_setting(ssm_client, fake_boto_client):
+    obj = SsmPreference(fake_boto_client, 'us-east-1')
+    assert obj.get_current_service_setting_check() is True
+    assert obj.update_public_service_setting_check('Disable') is True
+    assert obj.check_ssm_preferences() is None
