773f47f2982f06bb2caf4982525c64335e77395d
Anon4Now <tutkoda@gmail.com>
2022-10-10 21:29:31 -0400

    refactored tests for 89% cov

--- tests/test_resource_updates.py
+++ tests/test_resource_updates.py
@@ -5,7 +5,6 @@ from delete_aws_resources_with_py.resource_updates import (
     UpdateNaclResource,
     UpdateSgResource
 )
-from delete_aws_resources_with_py.main import execute_changes_on_resources
 
 
 def test_update_class_nacl_rules(get_resource_obj):
@@ -13,17 +12,9 @@ def test_update_class_nacl_rules(get_resource_obj):
     assert update_res.update_nacl_rules() is True
 
 
-# def test_update_sg_rules(get_resource_obj, mocker, egress_rule, ssm_client):
-#     def mock_get_sg_rules(*args, **kwargs):
-#         return egress_rule
-#
-#     mocker.patch('delete_aws_resources_with_py.main.UpdateSgResource.get_sg_rules', mock_get_sg_rules)
-#
-#     def mock_revoke_sg_rule(*args, **kwargs):
-#         return {'Return': True}
-#
-#     mocker.patch('delete_aws_resources_with_py.main.UpdateSgResource.revoke_ingress_sg_rule', mock_revoke_sg_rule)
-#     mocker.patch('delete_aws_resources_with_py.main.UpdateSgResource.revoke_egress_sg_rule', mock_revoke_sg_rule)
-#
-#     actual = execute_changes_on_resources('modify', get_resource_obj, ssm_client, 'us-east-1')
-#     assert actual is True
+def test_update_class_sg_rules(fake_resource_obj, fake_boto_client):
+    fake_data = {'something': 'anything'}
+    upd_sg = UpdateSgResource(fake_resource_obj)
+    assert upd_sg.get_sg_rules('sg-04f64a24f0250f7e5') == 'sgr-0f5d697d6bde6f9da'
+    assert upd_sg.revoke_egress_sg_rule(fake_data) is True
+    assert upd_sg.revoke_ingress_sg_rule(fake_data) is True
